a5ecb4e936f7fc1b759ace20bea03c70ace06e62
ACAVAGNE <91544506+WeZeOx@users.noreply.github.com>
2022-10-07 17:29:12 +0200

    add: check if comment is created but miss redirection

--- src/plugin.php
+++ src/plugin.php
@@ -2,6 +2,13 @@
 
 require __DIR__ . "/utils/consoleLog.php";
 
-function launchPLugin() {
-    console_log(get_comments());
+function launchPLugin()
+{
+    global $wp;
+    $query = add_query_arg($wp->query_vars, home_url($wp->request));
+
+    if (str_contains($query, "wp-comments-post.php")) {
+        wp_redirect("https://laphant.tonycava.dev/", 303);
+        exit;
+    }
 }
