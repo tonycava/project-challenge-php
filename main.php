1896a672134a5916a7a08bc1a06e7e16f24bce96
ACAVAGNE <91544506+WeZeOx@users.noreply.github.com>
2022-10-07 14:28:44 +0200

    add: boilerplate for wordpress plugin

--- main.php
+++ main.php
@@ -1,1 +1,12 @@
 <?php
+
+require __DIR__ . "/src/plugin.php";
+
+/**
+ * Plugin Name: Laphant
+ * Description: Laphant.
+ * Version: 0.1
+ * Author: Laphant
+ **/
+
+launchPLugin();
---
+++ src/plugin.php
@@ -0,0 +1,7 @@
+<?php
+
+require __DIR__ . "/utils/consoleLog.php";
+
+function launchPLugin() {
+    console_log(get_comments());
+}
