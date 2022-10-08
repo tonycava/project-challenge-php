c1ded33608ebb13fc70e72fb644438e6ab112e1e
ACAVAGNE <91544506+WeZeOx@users.noreply.github.com>
2022-10-08 17:37:46 +0200

    add: clean code for webhook

--- api/bot.php
+++ api/bot.php
@@ -7,8 +7,6 @@ use Discord\WebSockets\Intents;
 require_once('vendor/autoload.php');
 require_once('utils/token.php');
 
-$discord = "";
-
 function launchDiscordBot(): void
 {
     $key = getEnvironmentVariable('DISCORD_TOKEN');
@@ -23,7 +21,6 @@ function launchDiscordBot(): void
         $discord->on('ready', function (Discord $discord) {
             echo "bot is ready";
 
-
             echo "heartbeat called at: " . time() . PHP_EOL;
         });
 
@@ -33,28 +30,3 @@ function launchDiscordBot(): void
 }
 
 launchDiscordBot();
-
-
-$client = new React\Http\Browser();
-
-$http = new React\Http\HttpServer(function (Psr\Http\Message\ServerRequestInterface $request) {
-    return React\Http\Message\Response::plaintext(
-        "Hello World!\n"
-    );
-});
-
-$socket = new React\Socket\SocketServer('0.0.0.0:8080');
-$http->listen($socket);
-
-
-function test()
-{
-    $key = getEnvironmentVariable('DISCORD_TOKEN');
-    $discord = new Discord([
-        'token' => $key,
-    ]);
-
-    $guild = $discord->guilds->get('id', '917437857243734067');
-    $DiscordChannel = $guild->channels->get('id', '1027847561308016650');
-    $DiscordChannel->sendMessage('message');
-}
--- api/public/index.php
+++ api/public/index.php
@@ -5,6 +5,7 @@ use Psr\Http\Message\ServerRequestInterface as Request;
 use Slim\Factory\AppFactory;
 
 require_once './vendor/autoload.php';
+require_once('api/utils/token.php');
 
 $app = AppFactory::create();
 
@@ -12,16 +13,14 @@ $app->addErrorMiddleware(true, true, true);
 
 $app->get('/', function (Request $request, Response $response) {
 
-    $url = "https://discord.com/api/webhooks/1028317447842971700/qVt9oAYzupvit5Pd4PtNs07rTbTexctbUcdsDzNBPNK7wMjRrnrLj-q9XSgAmXEHgv5u";
-
     $headers = ['Content-Type: application/json; charset=utf-8'];
     $POST = ['username' => 'Testing BOT', 'content' => 'Testing message'];
 
     $ch = curl_init();
-    curl_setopt($ch, CURLOPT_URL, $url);
+    curl_setopt($ch, CURLOPT_URL, getEnvironmentVariable("WEB_HOOK_URL"));
     curl_setopt($ch, CURLOPT_POST, true);
     curl_setopt($ch, CURLOPT_HTTPHEADER, $headers);
-    curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+
     curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, false);
     curl_setopt($ch, CURLOPT_POSTFIELDS, json_encode($POST));
 
