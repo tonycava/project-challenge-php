3c27487d8ed80b7c3f92aa2e7b8700033c11856b
LESCAFFRE <lucas.escaffre@ynov.com>
2022-10-10 15:11:18 +0200

    continue: swearwords add to know if the user is angry or not add: swearWords.json refactor by adding a new directory for bot section

--- bot/bot.php
+++ bot/bot.php
@@ -14,7 +14,6 @@ $sad = ["unhappy", "sorrowful", "dejected", "depressed"];
 function launchDiscordBot(): void
 {
     $key = getEnvironmentVariable('DISCORD_TOKEN');
-
     echo $key;
 
     try {
@@ -23,12 +22,16 @@ function launchDiscordBot(): void
             'intents' => Intents::getDefaultIntents() | Intents::MESSAGE_CONTENT
         ]);
 
+
+
         $discord->on('ready', function (Discord $discord) {
             echo "bot is ready";
             $discord->on('message', function ($message, $discord) {
+
                 $contentMessage = $message->content;
                 $guild = $discord->guilds->get('id', '917437857243734067');
                 $DiscordChannel = $guild->channels->get('id', '1027847561308016650');
+
                 if ($contentMessage === '!joke') {
                     $client = new \GuzzleHttp\Client();
                     $response = $client->request('GET', 'https://api.chucknorris.io/jokes/random', ['verify' => false]);
@@ -39,8 +42,10 @@ function launchDiscordBot(): void
                     });
                     $message->reply($joke);
                 }
-                if ($message->author->username === 'Testing Bot') {
-                    if (ctype_upper($message->content)) {
+                if ($message->author->username === 'M4cht L3gend') {
+                    $numberOfSwear = takeDataFromJson($message, './swearWords.json');
+                    echo "$numberOfSwear";
+                    if (ctype_upper(str_replace(' ', '', $message->content)) || $numberOfSwear > 0) {
                         $DiscordChannel->sendMessage(":thumbsdown:");
                     } else {
                         $DiscordChannel->sendMessage(":thumbsup:");
@@ -54,6 +59,26 @@ function launchDiscordBot(): void
         $discord->run();
     } catch (\Discord\Exceptions\IntentException $e) {
     }
+}
+
+function takeDataFromJson(\Discord\Parts\Channel\Message $message, string $filePath): int
+{
+    $number = 0;
+    $json = file_get_contents("$filePath");
+    $json_data = json_decode($json, true);
+    print_r($json_data);
+    echo count($json_data);
+    echo "------";
+    echo $message->content;
+    echo "\n";
+    for ($honor = 0; $honor < count($json_data); $honor++) {
+        echo  "$message->content | $json_data[$honor] | " . strpos($message->content, $json_data[$honor]) . "\n";
+        if (strpos($message->content, $json_data[$honor]) !== false) {
+            echo "FOUND $message->content\n";
+            $number++;
+        }
+    }
+    return $number;
 }
 
 launchDiscordBot();
