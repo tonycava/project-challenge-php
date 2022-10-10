e1b72b1a8b80568b3f277d526c18dd7a62ae734a
LESCAFFRE <lucas.escaffre@ynov.com>
2022-10-10 12:31:30 +0200

    add: reply to message with reaction add jokes when user type !joke and start emotions filter

--- api/bot.php
+++ api/bot.php
@@ -7,6 +7,10 @@ use Discord\WebSockets\Intents;
 require_once('vendor/autoload.php');
 require_once('utils/token.php');
 
+$happy = ["elated", "excited", "happy", "joy"];
+$fear = ["nervous", "terrified", "scared"];
+$sad = ["unhappy", "sorrowful", "dejected", "depressed"];
+
 function launchDiscordBot(): void
 {
     $key = getEnvironmentVariable('DISCORD_TOKEN');
@@ -16,10 +20,33 @@ function launchDiscordBot(): void
     try {
         $discord = new Discord([
             'token' => $key,
+            'intents' => Intents::getDefaultIntents() | Intents::MESSAGE_CONTENT
         ]);
 
         $discord->on('ready', function (Discord $discord) {
             echo "bot is ready";
+            $discord->on('message', function ($message, $discord) {
+                $contentMessage = $message->content;
+                $guild = $discord->guilds->get('id', '917437857243734067');
+                $DiscordChannel = $guild->channels->get('id', '1027847561308016650');
+                if ($contentMessage === '!joke') {
+                    $client = new \GuzzleHttp\Client();
+                    $response = $client->request('GET', 'https://api.chucknorris.io/jokes/random', ['verify' => false]);
+                    $joke = json_decode($response->getBody());
+                    $joke = $joke->value;
+                    $message->react('👎')->done(function () {
+                        echo "";
+                    });
+                    $message->reply($joke);
+                }
+                if ($message->author->username === 'Testing Bot') {
+                    if (ctype_upper($message->content)) {
+                        $DiscordChannel->sendMessage(":thumbsdown:");
+                    } else {
+                        $DiscordChannel->sendMessage(":thumbsup:");
+                    }
+                }
+            });
 
             echo "heartbeat called at: " . time() . PHP_EOL;
         });
