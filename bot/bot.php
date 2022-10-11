7cb2d79adcafd8ad6f21e6014867a3b9f8a49961
LESCAFFRE <lucas.escaffre@ynov.com>
2022-10-11 14:42:36 +0200

    fix: change from last commit

--- bot/bot.php
+++ bot/bot.php
@@ -17,9 +17,28 @@ function launchDiscordBot(): void
         $discord->on('ready', function (Discord $discord) {
             echo "bot is ready";
 
-            $guild = $discord->guilds->get('id', '917437857243734067');
-            $DiscordChannel = $guild->channels->get('id', '1027847561308016650');
-            $DiscordChannel->sendMessage('up adn running on vps');
+            $discord->on('message', function ($message, $discord) {
+                $contentMessage = $message->content;
+                $guild = $discord->guilds->get('id', '917437857243734067');
+                $DiscordChannel = $guild->channels->get('id', '1027847561308016650');
+                if ($contentMessage === '!joke') {
+                    $client = new \GuzzleHttp\Client();
+                    $response = $client->request('GET', 'https://api.chucknorris.io/jokes/random', ['verify' => false]);
+                    $joke = json_decode($response->getBody());
+                    $joke = $joke->value;
+                    $message->react('👎')->done(function () {
+                        echo "";
+                    });
+                    $message->reply($joke);
+                }
+                if ($message->author->username === 'Testing Bot') {
+                    if (ctype_upper($message->content)) {
+                        $DiscordChannel->sendMessage(":thumbsdown:");
+                    } else {
+                        $DiscordChannel->sendMessage(":thumbsup:");
+                    }
+                }
+            });
 
             echo "heartbeat called at: " . time() . PHP_EOL;
         });
