fa0695c6a5db24a07a76487d69b99a1666f0e010
LESCAFFRE <lucas.escaffre@ynov.com>
2022-10-12 11:32:38 +0200

    fix: file_get_content error

--- api/send-message/telegram.php
+++ api/send-message/telegram.php
@@ -5,14 +5,14 @@ require_once('./vendor/autoload.php');
 $dotenv = Dotenv\Dotenv::createImmutable("./");
 $dotenv->load();
 
-function telegramSendMessage(mixed $dataMessage): void
+function telegramSendMessage(mixed $message): void
 {
 
     $apiToken = $_ENV['TELEGRAM_API_TOKEN'];
     $data = [
         'chat_id' => $_ENV['TELEGRAM_CHAT_ID'],
-        'text' => $dataMessage->comment_tittle
+        'text' => $message->comment_tittle
     ];
-    file_get_contents("https://api.send-message.org/bot$apiToken/sendMessage?" .
+    file_get_contents("https://api.telegram.org/bot$apiToken/sendMessage?" .
         http_build_query($data) );
 }
