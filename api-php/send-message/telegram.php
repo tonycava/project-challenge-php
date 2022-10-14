8dc2b55fa45271e275cea480bcc4b5fb4bf326c7
ACAVAGNE <91544506+WeZeOx@users.noreply.github.com>
2022-10-14 09:23:56 +0200

    fix: new message on telegram

--- api-php/send-message/telegram.php
+++ api-php/send-message/telegram.php
@@ -11,8 +11,8 @@ function telegramSendMessage(mixed $message): void
     $apiToken = $_ENV['TELEGRAM_API_TOKEN'];
     $data = [
         'chat_id' => $_ENV['TELEGRAM_CHAT_ID'],
-        'text' => $message->comment_tittle
+        'text' => "Comment : $message->comment_tittle\n\nGo moderate this new comment : https://laphant.tonycava.dev/wp-admin/edit-comments.php By : $message->comment_author\n\n At : $message->comment_date"
     ];
     file_get_contents("https://api.telegram.org/bot$apiToken/sendMessage?" .
-        http_build_query($data) );
+        http_build_query($data));
 }
