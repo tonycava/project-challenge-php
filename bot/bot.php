693e48133cb96ba8ad0a6b6564986a23fc7262cb
ACAVAGNE <91544506+WeZeOx@users.noreply.github.com>
2022-10-28 11:36:30 +0200

    test: get latest message and verify that reaction is added by human

--- bot/bot.php
+++ bot/bot.php
@@ -20,13 +20,14 @@ function launchDiscordBot(): void
 
         $discord->on('ready', function (Discord $discord) {
 
-            $discord->on("MESSAGE_REACTION_ADD", function ( $reaction, Discord $discord) {
+            $discord->on("MESSAGE_REACTION_ADD", function ($reaction, Discord $discord) {
                 echo "\n\n";
                 $guild = $discord->guilds->get('id', '917437857243734067');
                 $discordChannel = $guild->channels->get('id', '1027847561308016650');
 
                 $reaction->fetch()->done(function ($done) use ($reaction, $discord, $discordChannel) {
                     var_dump($done->message->author->bot);
+                    var_dump($done->count);
                     var_dump($reaction->count);
                     var_dump($done->emoji->name == "❌");
                     var_dump($done->emoji->name == "✔");
