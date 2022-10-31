e7d9a281bf9baf448673ac34321aa69dbe452f65
ACAVAGNE <91544506+WeZeOx@users.noreply.github.com>
2022-10-31 18:37:11 +0100

    test: refactor of bot.php

--- bot/bot.php
+++ bot/bot.php
@@ -2,31 +2,37 @@
 
 use Discord\Builders\MessageBuilder;
 use Discord\Discord;
+use Discord\Exceptions\IntentException;
+use Discord\Parts\WebSockets\MessageReaction;
+use Discord\WebSockets\Event;
+use Discord\Parts\Channel\Message;
+use Dotenv\Dotenv;
+use GuzzleHttp\Client;
+use GuzzleHttp\RequestOptions;
 
 require_once('./vendor/autoload.php');
 
-$dotenv = Dotenv\Dotenv::createImmutable("./");
+$dotenv = Dotenv::createImmutable("./");
 $dotenv->load();
 
 const BOT_USERNAME = "LAphant de wish";
 
 function launchDiscordBot(): void
 {
-
   try {
     $discord = new Discord([
       'token' => $_ENV['DISCORD_TOKEN'],
     ]);
 
-    $discord->on('ready', function (Discord $discord) {
-      $discord->on("MESSAGE_REACTION_ADD", function (\Discord\Parts\WebSockets\MessageReaction $reaction, Discord $discord) {
-        echo "\n\n";
-        $guild = $discord->guilds->get('id', '917437857243734067');
-        $discordChannel = $guild->channels->get('id', '1027847561308016650');
+    $discord->on(Event::READY, function (Discord $discord) {
+      $guild = $discord->guilds->get('id', '917437857243734067');
+      $discordChannel = $guild->channels->get('id', '1027847561308016650');
 
+      $discord->on(Event::MESSAGE_REACTION_ADD, function (MessageReaction $reaction, Discord $discord) use ($discordChannel) {
         $reaction->fetch()->then(function ($done) use ($reaction, $discord, $discordChannel) {
           $cross = $reaction->message->reactions->get("id", "❌")->count == null ? 0 : $reaction->message->reactions->get("id", "❌")->count;
           $valid = $reaction->message->reactions->get("id", "✔")->count == null ? 0 : $reaction->message->reactions->get("id", "✔")->count;
+
           var_dump($cross);
           var_dump($valid);
 
@@ -34,63 +40,46 @@ function launchDiscordBot(): void
             $discordChannel->getMessageHistory([
               'before' => $done->message->id,
               'limit' => 1,
-            ])->done(function ($messages) use ($reaction, $done, $discord) {
+            ])->done(function ($messages) use ($reaction, $done) {
               foreach ($messages as $message) {
                 $array = explode(" ", $message->content);
                 $last = end($array);
                 $commentId = str_replace("#", "", $last);
-
-                $link = new mysqli("wordpress_db:3306", "username", "password", "wordpress");
-                if (!$link) {
-                  echo "Error: Unable to connect to MySQL." . PHP_EOL;
-                  echo "Debugging errno: " . mysqli_connect_errno() . PHP_EOL;
-                  echo "Debugging error: " . mysqli_connect_error() . PHP_EOL;
-                  exit;
-                }
+                $link = new mysqli("wordpress_db:3306", "username", "password", "wordpress") or die("Error when connecting to database");
 
                 if ($done->emoji->name == "❌") {
-                  $link->query("UPDATE wp_comments SET comment_approved = \"trash\" WHERE comment_ID = $commentId")->fetch_assoc();
+                  $link->query(/** @lang sql */ "UPDATE wp_comments SET comment_approved = \"trash\" WHERE comment_ID = $commentId")->fetch_assoc();
                   $reaction->channel->editMessage($reaction->message, MessageBuilder::new()->setContent("Do you approve this comment or not ? (Already approved or in trash)"));
                 } elseif ($done->emoji->name == "✔") {
-                  $link->query("UPDATE wp_comments SET comment_approved = 1 WHERE comment_ID LIKE $commentId")->fetch_assoc();
+                  $link->query(/** @lang sql */ "UPDATE wp_comments SET comment_approved = 1 WHERE comment_ID LIKE $commentId")->fetch_assoc();
                   $reaction->channel->editMessage($reaction->message, MessageBuilder::new()->setContent("Do you approve this comment or not ? (Already approved or in trash)"));
                 }
-
-                echo "Success: A proper connection to MySQL was made!" . PHP_EOL;
-                echo "Host information: " . mysqli_get_host_info($link) . PHP_EOL;
                 mysqli_close($link);
               }
             });
           }
         });
-        echo "\n\n";
       });
-
-      $discord->on("message", function ($message, Discord $discord) {
-        $contentMessage = $message->content;
-        $guild = $discord->guilds->get('id', '917437857243734067');
-        if ($contentMessage === '!joke') {
-          $client = new \GuzzleHttp\Client();
+      $discord->on(Event::MESSAGE_CREATE, function (Message $message, Discord $discord) use ($discordChannel) {
+        if ($message->content === '!joke') {
+          $client = new Client();
           $response = $client->request('GET', 'https://api.chucknorris.io/jokes/random', ['verify' => false]);
-          $joke = json_decode($response->getBody());
-          $joke = $joke->value;
+          $joke = json_decode($response->getBody())->value;
           $message->reply($joke);
         }
 
         if ($message->author->username === BOT_USERNAME) {
-          $client = new \GuzzleHttp\Client();
+          $client = new Client();
           $response = $client->post('https://api.emotion.laphant.tonycava.dev/get-emotion', [
             'verify' => false,
-            \GuzzleHttp\RequestOptions::JSON => ['emotion' => $message->content]
+            RequestOptions::JSON => ['emotion' => $message->content]
           ]);
 
           $emotionResponse = json_decode($response->getBody());
           if ($emotionResponse->emotion == ":(") $message->react('👎');
           else $message->react('👍');
 
-          $guild = $discord->guilds->get('id', '917437857243734067');
-          $discordChannel = $guild->channels->get('id', '1027847561308016650');
-          $discordChannel->sendMessage("Do you approve this comment or not ?")->done(function (\Discord\Parts\Channel\Message $message) {
+          $discordChannel->sendMessage("Do you approve this comment or not ?")->done(function (Message $message) {
             $message->react('✔');
             $message->react('❌');
           });
@@ -99,7 +88,8 @@ function launchDiscordBot(): void
       echo "heartbeat called at: " . time() . PHP_EOL;
     });
     $discord->run();
-  } catch (\Discord\Exceptions\IntentException $e) {
+  } catch (IntentException $e) {
+    echo "Error while connecting to discord :" . $e->getMessage();
   }
 }
 
